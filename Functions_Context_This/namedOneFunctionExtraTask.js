50e5e8282999e98c33c6d56aa1401026873ee4e9
Artyom Borushko <artsem_barushka@epam.com>
2021-12-09 19:33:27 +0300

    update getAverageAge functionality

--- Functions_Context_This/namedOneFunctionExtraTask.js
+++ Functions_Context_This/namedOneFunctionExtraTask.js
@@ -1,7 +1,11 @@
 
 
 function NamedOne(fName, lName, personAge) {
-    
+
+    (function () {
+        if (personAge) NamedOne.ageOfAllInstances.push(personAge);
+    })();
+
     return {
         fName,
         lName,
@@ -34,15 +38,21 @@ function NamedOne(fName, lName, personAge) {
         },
     };
 };
-
+NamedOne.ageOfAllInstances = [];
+NamedOne.getAverageAge = function() {
+    return NamedOne.ageOfAllInstances.reduce((prevValue, currValue) => prevValue + currValue, 0) / NamedOne.ageOfAllInstances.length;
+};
 
 const namedTwo = new NamedOne("Naomi", "Wang", 20);
-const namedThree = new NamedOne("Wang", "Naomi", 2);
+console.log(NamedOne.getAverageAge())
 
-NamedOne.getAverageAge = function(...users) {
-    return users.reduce((accumulator, user) => accumulator + user.age, 0) / users.length;
-};
-// console.log(NamedOne.getAverageAge(namedTwo, namedThree));
+const namedThree = new NamedOne("Wang", "Naomi", 2);
+console.log(NamedOne.getAverageAge())
+
+// NamedOne.getAverageAgeByInstances = function(...users) {
+//     return users.reduce((accumulator, user) => accumulator + user.age, 0) / users.length;
+// };
+// console.log(NamedOne.getAverageAgeByInstances(namedTwo, namedThree));
 
 // const namedOne = new NamedOne("Naomi");
 // namedOne.fullName;
